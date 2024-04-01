62dccbb9ea7ea43d0d46c3805ce91458956042c4
gercokim <gckim@andrew.cmu.edu>
2024-04-01 12:48:54 -0400

    Cleaned up mock test file

--- test/mock/test_mock.py
+++ test/mock/test_mock.py
@@ -4,10 +4,8 @@ from src.translator import translate_content
 
 @patch('vertexai.preview.language_models._PreviewChatSession.send_message')
 def test_unexpected_language(mocker):
-  # we mock the model's response to return a random message
   mocker.return_value.text = "I don't understand your request"
   content = "Aquí está su primer ejemplo." 
-  # TODO assert the expected behavior
   assert translate_content(content) == (True, content)
 
 @patch('vertexai.preview.language_models._PreviewChatSession.send_message')
